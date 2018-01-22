a0e82f38fa40287de98809cde103dee77cf6fad1
Dave Ogle <dogle@scottlogic.co.uk>
2018-01-22 14:39:14 +0000

    First Euler problem and setup

---
+++ src/com/euler/main.java
@@ -0,0 +1,8 @@
+package com.euler;
+
+public class main {
+    public static void main(String[] args) {
+        Problem2 problem = new Problem2();
+        System.out.println("Problem answer: " + problem.run());
+    }
+}
